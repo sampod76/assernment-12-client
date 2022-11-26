5c17d38e6487de258d57438990d24430a6eae858
sampod76 <108483839+sampod76@users.noreply.github.com>
2022-11-27 05:40:36 +0600

    payment page add

---
+++ src/component/Products/Add-Payment/Payment.js
@@ -0,0 +1,66 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import React from 'react';
+import toast from 'react-hot-toast';
+import { ThreeCircles } from 'react-loader-spinner';
+import { useQuery } from 'react-query';
+import { useLocation, useParams } from 'react-router-dom';
+import ChackoutForm from './ChackoutFrom';
+// const stripePromise = loadStripe(process.env.REACT_STRIPE_PUBLISHABLE_KEY);
+const stripePromise = loadStripe("pk_test_51M6MdfAdJu4EQtRS8tBWituEbmkenJzMLqByrl2hBhZ47U8p8sGAKdC05mwlEiZVCNntARDELfFH7xjofCb8Z99N004Y63poiB");
+
+const Payment = () => {
+    //     const{state}=useLocation()
+    //  const {model,price}=state.selinfo
+    const { id } = useParams()
+
+    const { data: bookingData = {}, isLoading, refetch } = useQuery({
+        queryKey: ['bookingData'],
+        queryFn: async () => {
+            const res = await fetch(`http://localhost:5000/booking/${id}`)
+            const result = await res.json()
+            if (result.success) {
+                toast.success('booking data gats')
+                return result.data
+            } else {
+                toast.error(result.message)
+            }
+        }
+    })
+
+    console.log(bookingData);
+
+   
+
+
+    if (isLoading) {
+        return <div className='flex justify-center items-center min-h-screen'>
+            <ThreeCircles
+                height="200"
+                width="200"
+                color="#4fa94d"
+                wrapperStyle={{}}
+                wrapperclass=""
+                visible={true}
+                ariaLabel="three-circles-rotating"
+                outerCircleColor=""
+                innerCircleColor=""
+                middleCircleColor=""
+            />
+        </div>
+    }
+  
+
+    return (
+        <div>
+            <h1 className='text-xl md:text-4xl font-bold'>Mobile : {bookingData?.model}</h1>
+            <h1 className='text-xl md:text-4xl font-bold'>Please payment : {bookingData?.sellerInfo?.selarPrice} tk</h1>
+
+            <Elements stripe={stripePromise}>
+                <ChackoutForm bookingData={bookingData} />
+            </Elements>
+        </div>
+    );
+};
+
+export default Payment;
